891335024537265a5c8696392dffe565f449dcd7
xbpneus-gestao-de-frota <219038582+xbpneus-gestao-de-frota@users.noreply.github.com>
2025-10-10 15:49:53 -0400

    Fix: Criar superusuário no build.sh usando email como identificador

--- build.sh
+++ build.sh
@@ -19,11 +19,10 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@xbpneus.com')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Teste@2025')
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@xbpneus.com")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "Teste@2025")
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password)
 EOF
 

f9905854d7ba9b76c3d490f68126a40a66505330
xbpneus-gestao-de-frota <219038582+xbpneus-gestao-de-frota@users.noreply.github.com>
2025-10-17 11:38:24 -0400

    Fix: Configura tempos de vida dos tokens JWT no settings.py

--- config/settings.py
+++ config/settings.py
@@ -265,3 +265,12 @@ AUDIT_MASK_FIELDS = {
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': True,
+}
